8901960f1489c6ca100e2f247ae941454740b8c8
Arthur Jamet <arthur.jamet@gmail.com>
2024-03-22 08:23:32 +0100

    Scanner: Use API KEY from env

--- scanner/config.go
+++ scanner/config.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"flag"
+	"github.com/kpango/glg"
+	"os"
 )
 
 type Config struct {
 	// The Directory to watch
 	WatchDir string
+	// Secret API Key to authenticate
+	ApiKey string
 }
 
-// Parses and return a config from the CLI args
+// Parses and return a config from the CLI args and env args
 func get_config() Config {
 	var config Config
 	watchDir := flag.String("d", "", "the directory to watch")
 	flag.Parse()
 
+	apiKey, is_present := os.LookupEnv("SCANNER_API_KEY")
+
+	if !is_present || len(apiKey) == 0 {
+		glg.Fatalf("SCANNER_API_KEY is missing or empty.")
+		os.Exit(1)
+	}
+
+	config.ApiKey = apiKey
 	config.WatchDir = *watchDir
 
 	return config
