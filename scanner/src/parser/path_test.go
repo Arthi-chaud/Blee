d73ce740fc7f8f2a84d7ed293759a77fde79ca17
Arthur Jamet <arthur.jamet@gmail.com>
2024-03-26 18:25:27 +0100

    Scanner: Tests for Path Metadata parsing

--- scanner/src/parser/path_test.go
+++ scanner/src/parser/path_test.go
@@ -27,14 +27,110 @@ func TestMovieParsingWithoutPackageFolderWithYear(t *testing.T) {
 	path := "/videos/The Corrs - Unplugged (1999).mkv"
 	res, err := ParseMetadataFromPath(path, config)
 
-	assert.Equal(t, err, nil, "Parsing Failed")
-	assert.Equal(t, res.extra, nil, "Expected Movie, got extra")
-	assert.Equal(t, res.movie.artist_name, "The Corrs", "Wrong Artist Name")
-	assert.Equal(t, res.movie.name, "Unplugged", "Wrong Movie Name")
-	assert.Equal(t, res.movie.type_, models.Concert, "Wrong Movie Type")
+	assert.Nil(t, err)
+	assert.Nil(t, res.extra)
+	assert.Equal(t, res.movie.artist_name, "The Corrs")
+	assert.Equal(t, res.movie.name, "Unplugged")
+	assert.Equal(t, res.movie.type_, models.Concert)
 	assert.Equal(t, res.movie.package_.artist_name, "The Corrs")
 	assert.Equal(t, res.movie.package_.name, "Unplugged")
 	assert.Equal(t, res.movie.package_.release_year.Year(), 1999)
 }
 
+func TestMovieParsingWithPackageFolderWithoutYear(t *testing.T) {
+	config := getTestConfig()
+	path := "/videos/The Corrs - Unplugged/The Corrs - Unplugged.mkv"
+	res, err := ParseMetadataFromPath(path, config)
 
+	assert.Nil(t, err)
+	assert.Nil(t, res.extra)
+	assert.Equal(t, res.movie.artist_name, "The Corrs")
+	assert.Equal(t, res.movie.name, "Unplugged")
+	assert.Equal(t, res.movie.type_, models.Concert)
+	assert.Equal(t, res.movie.package_.artist_name, "The Corrs")
+	assert.Equal(t, res.movie.package_.name, "Unplugged")
+	assert.Equal(t, res.movie.package_.release_year, time.Time{})
+}
+
+func TestMovieParsingWithPackageFolderWithArtistFolderWithMovieYear(t *testing.T) {
+	config := getTestConfig()
+	path := "/videos/The Corrs/Unplugged (1999)/The Corrs - Unplugged.mkv"
+	res, err := ParseMetadataFromPath(path, config)
+
+	assert.Nil(t, err)
+	assert.Nil(t, res.extra)
+	assert.Equal(t, res.movie.artist_name, "The Corrs")
+	assert.Equal(t, res.movie.name, "Unplugged")
+	assert.Equal(t, res.movie.type_, models.Concert)
+	assert.Equal(t, res.movie.package_.artist_name, "The Corrs")
+	assert.Equal(t, res.movie.package_.name, "Unplugged")
+	assert.Equal(t, res.movie.package_.release_year.Year(), 1999)
+}
+
+func TestExtraParsingWithPackageFolderWithoutArtistFolder(t *testing.T) {
+	config := getTestConfig()
+	path := "/videos/Olivia Ruiz - Miss Météores Live (2008)/Autour D'Olivia (Documentaire)-interview.mkv"
+	res, err := ParseMetadataFromPath(path, config)
+
+	assert.Nil(t, err)
+	assert.Nil(t, res.movie)
+	assert.Equal(t, res.extra.artist_name, "Olivia Ruiz")
+	assert.Equal(t, res.extra.disc_index, 0)
+	assert.Equal(t, res.extra.track_index, 0)
+	assert.Equal(t, res.extra.name, "Autour D'Olivia (Documentaire)")
+	assert.Equal(t, res.extra.types, []models.ExtraType {models.Interview})
+	assert.Equal(t, res.extra.package_.artist_name, "Olivia Ruiz")
+	assert.Equal(t, res.extra.package_.name, "Miss Météores Live")
+	assert.Equal(t, res.extra.package_.release_year.Year(), 2008)
+}
+
+func TestExtraParsingWithPackageFolderWithArtistFolder(t *testing.T) {
+	config := getTestConfig()
+	path := "/videos/Olivia Ruiz/Chocolat Show/Interview-interview.mkv"
+	res, err := ParseMetadataFromPath(path, config)
+
+	assert.Nil(t, err)
+	assert.Nil(t, res.movie)
+	assert.Equal(t, res.extra.artist_name, "Olivia Ruiz")
+	assert.Equal(t, res.extra.disc_index, 0)
+	assert.Equal(t, res.extra.track_index, 0)
+	assert.Equal(t, res.extra.name, "Interview")
+	assert.Equal(t, res.extra.types, []models.ExtraType {models.Interview})
+	assert.Equal(t, res.extra.package_.artist_name, "Olivia Ruiz")
+	assert.Equal(t, res.extra.package_.name, "Chocolat Show")
+	assert.Equal(t, res.extra.package_.release_year, time.Time{})
+}
+
+func TestExtraParsingWithPackageFolderWithArtistFolderWithExtraFolder(t *testing.T) {
+	config := getTestConfig()
+	path := "/videos/Olivia Ruiz/Chocolat Show/Extras/Interview-interview.mkv"
+	res, err := ParseMetadataFromPath(path, config)
+
+	assert.Nil(t, err)
+	assert.Nil(t, res.movie)
+	assert.Equal(t, res.extra.artist_name, "Olivia Ruiz")
+	assert.Equal(t, res.extra.disc_index, 0)
+	assert.Equal(t, res.extra.track_index, 0)
+	assert.Equal(t, res.extra.name, "Interview")
+	assert.Equal(t, res.extra.types, []models.ExtraType {models.Interview})
+	assert.Equal(t, res.extra.package_.artist_name, "Olivia Ruiz")
+	assert.Equal(t, res.extra.package_.name, "Chocolat Show")
+	assert.Equal(t, res.extra.package_.release_year, time.Time{})
+}
+
+func TestExtraParsingWithPackageFolderWithoutArtistFolderWithIndexesWithoutTypes(t *testing.T) {
+	config := getTestConfig()
+	path := "/videos/Artist - Package (2000)/1-02 Extra1.mkv"
+	res, err := ParseMetadataFromPath(path, config)
+
+	assert.Nil(t, err)
+	assert.Nil(t, res.movie)
+	assert.Equal(t, res.extra.artist_name, "Artist")
+	assert.Equal(t, res.extra.disc_index, 1)
+	assert.Equal(t, res.extra.track_index, 2)
+	assert.Equal(t, res.extra.name, "Extra1")
+	assert.Equal(t, res.extra.types, []models.ExtraType {models.Other})
+	assert.Equal(t, res.extra.package_.artist_name, "Artist")
+	assert.Equal(t, res.extra.package_.name, "Package")
+	assert.Equal(t, res.extra.package_.release_year.Year(), 2000)
+}
