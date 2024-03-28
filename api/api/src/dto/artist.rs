122d702f3713ad78cbb8d8da8b37499792a8e509
Arthur Jamet <arthur.jamet@gmail.com>
2024-03-28 19:11:08 +0100

    API: ArtistSort

--- api/api/src/dto/artist.rs
+++ api/api/src/dto/artist.rs
@@ -1,12 +1,11 @@
 use super::image::ImageResponse;
 use crate::swagger_examples::*;
-use crate::utils::Identifiable;
 use chrono::NaiveDateTime;
 use entity::{artist, image};
 use rocket::serde::uuid::Uuid;
 use rocket_okapi::okapi::schemars;
 use rocket_okapi::okapi::schemars::JsonSchema;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 /// An ArtistResponse with the ImageResponse of the related poster, if there is
 /// one
@@ -15,12 +14,6 @@ pub struct ArtistWithPosterResponse {
 	#[serde(flatten)]
 	pub artist: ArtistResponse,
 	pub poster: Option<ImageResponse>,
-}
-
-impl Identifiable for ArtistWithPosterResponse {
-	fn get_id(&self) -> String {
-		self.artist.get_id()
-	}
 }
 
 impl From<(artist::Model, Option<image::Model>)> for ArtistWithPosterResponse {
@@ -48,12 +41,6 @@ pub struct ArtistResponse {
 	pub poster_id: Option<Uuid>,
 }
 
-impl Identifiable for ArtistResponse {
-	fn get_id(&self) -> String {
-		self.id.to_string()
-	}
-}
-
 impl From<artist::Model> for ArtistResponse {
 	fn from(value: artist::Model) -> Self {
 		ArtistResponse {
@@ -70,4 +57,11 @@ impl From<artist::Model> for ArtistResponse {
 // Filter for Artist
 pub struct ArtistFilter {
 	pub package: Option<Uuid>,
+}
+
+// Sorting for Artist
+#[derive(Deserialize, FromFormField, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ArtistSort {
+	Name
 }
