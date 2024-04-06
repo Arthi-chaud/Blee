76eb9f8b2fe4abffd3442463d71335060923966e
Arthur Jamet <arthur.jamet@gmail.com>
2024-04-06 09:27:05 +0200

    Scanner: Delete Missing files on Startup

--- scanner/pkg/actions/delete.go
+++ scanner/pkg/actions/delete.go
@@ -1,11 +1,25 @@
 package actions
 
 import (
+	"path/filepath"
+
+	"github.com/Arthi-chaud/Blee/scanner/pkg/api"
 	"github.com/Arthi-chaud/Blee/scanner/pkg/config"
 	"github.com/kpango/glg"
 )
 
 // Delete File from API
 func DeleteFile(path string, c *config.Config) {
-	glg.Logf("File to delete %s", path)
+	file, err := api.GetFileByPath(path, *c)
+	if err != nil {
+		glg.Fail("Getting File entry from API failed.")
+		glg.Fail(err.Error())
+		return
+	}
+	if err = api.DeleteFile(file.Id, *c); err != nil {
+		glg.Fail("Deleting File entry from API failed.")
+		glg.Fail(err.Error())
+		return
+	}
+	glg.Successf("File '%s' deleted", filepath.Base(path))
 }
