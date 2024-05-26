856c0930bebebdfea42dacfc943b22321abe9bc5
Arthur Jamet <arthur.jamet@gmail.com>
2024-05-26 17:34:45 +0200

    Front: Try to reduce complexity of grid tile size computation

--- front/lib/ui/src/infinite_scroll/utils.dart
+++ front/lib/ui/src/infinite_scroll/utils.dart
@@ -31,38 +31,33 @@ class InfiniteScrollHelper {
 const padding = 8.0;
 
 SliverGridDelegate DefaultThumbnailTileGridDelegate(BuildContext context) {
+  final bpData = ResponsiveBreakpoints.of(context);
   return SliverGridDelegateWithFixedCrossAxisCount(
     childAspectRatio: ThumbnailTile.aspectRatio,
     crossAxisSpacing: padding,
-    crossAxisCount: ResponsiveBreakpoints.of(context)
-            .largerOrEqualTo(BreakpointEnum.xl.name)
+    crossAxisCount: bpData.largerOrEqualTo(BreakpointEnum.xl.name)
         ? 5
-        : ResponsiveBreakpoints.of(context)
-                .largerOrEqualTo(BreakpointEnum.md.name)
+        : bpData.largerOrEqualTo(BreakpointEnum.md.name)
             ? 4
-            : ResponsiveBreakpoints.of(context)
-                    .largerOrEqualTo(BreakpointEnum.sm.name)
+            : bpData.largerOrEqualTo(BreakpointEnum.sm.name)
                 ? 3
                 : 2,
   );
 }
 
 SliverGridDelegate DefaultPosterTileGridDelegate(BuildContext context) {
+  final bpData = ResponsiveBreakpoints.of(context);
   return SliverGridDelegateWithFixedCrossAxisCount(
     childAspectRatio: PosterTile.aspectRatio,
     crossAxisSpacing: padding,
     mainAxisSpacing: padding,
-    crossAxisCount: ResponsiveBreakpoints.of(context)
-            .largerOrEqualTo(BreakpointEnum.xl.name)
+    crossAxisCount: bpData.largerOrEqualTo(BreakpointEnum.xl.name)
         ? 9
-        : ResponsiveBreakpoints.of(context)
-                .largerOrEqualTo(BreakpointEnum.lg.name)
+        : bpData.largerOrEqualTo(BreakpointEnum.lg.name)
             ? 8
-            : ResponsiveBreakpoints.of(context)
-                    .largerOrEqualTo(BreakpointEnum.md.name)
+            : bpData.largerOrEqualTo(BreakpointEnum.md.name)
                 ? 6
-                : ResponsiveBreakpoints.of(context)
-                        .largerOrEqualTo(BreakpointEnum.sm.name)
+                : bpData.largerOrEqualTo(BreakpointEnum.sm.name)
                     ? 4
                     : 3,
   );
