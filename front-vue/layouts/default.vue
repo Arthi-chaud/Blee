87026437319caf990bfad1e0e7acdc7cad745047
Arthur Jamet <arthur.jamet@gmail.com>
2024-07-18 18:25:41 +0200

    Front Vue: Appbar

---
+++ front-vue/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="ml-2 navbar bg-base-100">
+        <img src="/icon.png" class="h-10" />
+        <a class="ml-2 btn btn-ghost text-xl no-animation">Blee</a>
+    </div>
+    <slot />
+</template>
