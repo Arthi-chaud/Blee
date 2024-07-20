c2603041a8f3ee672fb49aa31a65f4a2a01afa10
Arthur Jamet <arthur.jamet@gmail.com>
2024-07-20 17:13:50 +0200

    Front Vue: Rework scrolling behaviour of global layout

--- front-vue/layouts/default.vue
+++ front-vue/layouts/default.vue
@@ -1,39 +1,41 @@
 <script setup lang="ts">
 const homeRoute = "/packages";
-const routes = ref([
+const routes = [
     { path: "/artists", label: "Artists", icon: "user" },
     { path: "/packages", label: "Movies", icon: "film" },
     { path: "/extras", label: "Videos", icon: "tv" },
     { path: "/settings", label: "Settings", icon: "cog" },
-]);
+] as const;
 const router = useRouter();
 const path = computed(() => router.currentRoute.value.path);
 </script>
 <template>
-    <div class="ml-2 navbar bg-base-100">
-        <NuxtLink :to="homeRoute">
-            <img src="/icon.png" class="h-10" />
-            <span class="ml-4 text-xl">Blee</span>
-        </NuxtLink>
-    </div>
-    <div class="w-full h-full flex flex-row">
-        <div class="hidden sm:flex">
-            <nav class="sidenav px-2">
-                <NuxtLink
-                    class="btn btn-ghost no-animation p-2 !h-auto flex flex-col mb-2"
-                    :class="{ 'font-normal': item.path != path }"
-                    v-for="item in routes"
-                    :to="item.path"
-                >
-                    <fa :icon="item.icon" />
-                    <span class="pt-0.5">
-                        {{ item.label }}
-                    </span>
-                </NuxtLink>
-            </nav>
+    <div class="h-screen w-screen overflow-clip">
+        <div class="pl-4 navbar bg-base-100">
+            <NuxtLink :to="homeRoute" class="navbar-start">
+                <img src="/icon.png" class="h-10" />
+                <span class="ml-4 text-xl">Blee</span>
+            </NuxtLink>
         </div>
-        <div class="w-full h-full">
-            <slot />
+        <div class="w-full h-full flex flex-row">
+            <div class="hidden sm:flex">
+                <nav class="sidenav px-2">
+                    <NuxtLink
+                        class="btn btn-ghost no-animation p-2 !h-auto flex flex-col mb-2"
+                        :class="{ 'font-normal': item.path != path }"
+                        v-for="item in routes"
+                        :to="item.path"
+                    >
+                        <fa :icon="item.icon" />
+                        <span class="pt-0.5">
+                            {{ item.label }}
+                        </span>
+                    </NuxtLink>
+                </nav>
+            </div>
+            <div class="w-full h-full overflow-scroll">
+                <slot />
+            </div>
             <div class="btm-nav sm:hidden">
                 <NuxtLink
                     :class="{ active: item.path == path }"
