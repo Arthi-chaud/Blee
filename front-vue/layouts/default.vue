561623778912b0bada5a4a554dd14ba431f24a4f
Arthur Jamet <arthur.jamet@gmail.com>
2024-07-20 15:45:50 +0200

    Front Vue: Use NuxtLinks

--- front-vue/layouts/default.vue
+++ front-vue/layouts/default.vue
@@ -11,40 +11,40 @@ const path = computed(() => router.currentRoute.value.path);
 </script>
 <template>
     <div class="ml-2 navbar bg-base-100">
-        <a :href="homeRoute">
+        <NuxtLink :to="homeRoute">
             <img src="/icon.png" class="h-10" />
             <span class="ml-4 text-xl">Blee</span>
-        </a>
+        </NuxtLink>
     </div>
     <div class="w-full h-full flex flex-row">
         <div class="hidden sm:flex">
             <nav class="sidenav px-2">
-                <a
+                <NuxtLink
                     class="btn btn-ghost no-animation p-2 !h-auto flex flex-col mb-2"
                     :class="{ 'font-normal': item.path != path }"
                     v-for="item in routes"
-                    :href="item.path"
+                    :to="item.path"
                 >
                     <fa :icon="item.icon" />
                     <span class="pt-0.5">
                         {{ item.label }}
                     </span>
-                </a>
+                </NuxtLink>
             </nav>
         </div>
         <div class="w-full h-full">
             <slot />
             <div class="btm-nav sm:hidden">
-                <a
+                <NuxtLink
                     :class="{ active: item.path == path }"
                     v-for="item in routes"
-                    :href="item.path"
+                    :to="item.path"
                 >
                     <fa :icon="item.icon" />
                     <span class="pt-0.5">
                         {{ item.label }}
                     </span>
-                </a>
+                </NuxtLink>
             </div>
         </div>
     </div>
