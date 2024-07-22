fbd0fa6a6e8f8ccb7a8990b23fedc3e76aa616c8
Arthur Jamet <arthur.jamet@gmail.com>
2024-07-22 18:34:00 +0200

    Front Vue: Image with blurhash transition

--- front-vue/components/image.vue
+++ front-vue/components/image.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { Image } from "~/models/domain/image";
-import { ref } from "vue";
+import { ref, type VNodeRef } from "vue";
 const { image } = defineProps<{
     expectedAspectRatio: number;
     image: Image | undefined | null;
@@ -8,7 +8,6 @@ const { image } = defineProps<{
     fitToExpectedAspectRatio?: true;
 }>();
 
-//todo blurhash
 const blurashURL = computed(() => blurHashToDataURL(image?.blurhash));
 
 const imageIsLoaded = ref(false);
@@ -22,7 +21,7 @@ const imageIsLoaded = ref(false);
         }"
     >
         <div
-            class="poster-rounded flex align-end justify-center h-full w-full"
+            class="poster-rounded flex align-end justify-center h-full w-full relative"
             :style="{
                 aspectRatio: image?.aspect_ratio,
             }"
@@ -36,11 +35,13 @@ const imageIsLoaded = ref(false);
                     }"
                     @load="imageIsLoaded = true"
                 />
-                <!-- <img
-                    v-if="blurashURL && !imageIsLoaded"
+                <img
+                    v-if="blurashURL"
                     :src="blurashURL"
-                    class="h-full w-full"
-                /> -->
+                    id="blurhashElemId"
+                    :style="{ zIndex: -1 }"
+                    class="h-full w-full absolute top-0"
+                />
             </template>
             <div
                 v-else-if="image === undefined"
