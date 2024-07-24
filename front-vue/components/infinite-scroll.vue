22f8d302c51ba8ecb259ec5ff60441b650b6a732
Arthur Jamet <arthur.jamet@gmail.com>
2024-07-24 10:53:06 +0200

    Front Vue: Infinte Scroll Component

---
+++ front-vue/components/infinite-scroll.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts" generic="T extends Resource">
+import { API } from "~/api/api";
+import type { Resource } from "~/models/domain/resource";
+import type { PaginatedQuery } from "~/models/queries";
+
+const { query } = defineProps<{
+    query: PaginatedQuery<T>;
+    type: "poster";
+}>();
+const slots = defineSlots<{
+    default(props: { item: T | undefined }): any;
+}>();
+const { data, hasNextPage, fetchNextPage } = useInfiniteQuery(query);
+
+const itemsCount = computed(
+    () =>
+        data.value?.pages
+            .map(({ items }) => items.length)
+            .reduce((a, b) => a + b, 0) ?? 0,
+);
+const getItem = computed(() => (itemIndex: number) => {
+    const item = data.value?.pages
+        .at(Math.floor(itemIndex / API.defaultPageSize))
+        ?.items.at(itemIndex % API.defaultPageSize);
+    return item;
+});
+const skeletons = computed(() => (hasNextPage ? [1, 2] : []));
+const _ = useInfiniteScroll(
+    //TODO Make this cleaner
+    document ? document.getElementById("el") : undefined,
+    async () => {
+        if (hasNextPage) {
+            await fetchNextPage();
+        }
+    },
+    { distance: 100, canLoadMore: () => hasNextPage.value },
+);
+</script>
+<template>
+    <div class="w-full h-auto flex justify-center">
+        <div class="poster-grid h-auto max-w-screen-xl p-2">
+            <slot
+                v-bind="{ item: getItem(n - 1) }"
+                v-for="n in itemsCount"
+            />
+            <slot
+                v-bind="{ item: undefined }"
+                v-if="hasNextPage"
+                v-for="_ in skeletons"
+            />
+        </div>
+    </div>
+</template>
--- front-vue/pages/packages/index.vue
+++ front-vue/pages/packages/index.vue
@@ -1,41 +1,11 @@
 <script setup lang="ts">
 import { API } from "~/api/api";
-import { useInfiniteQuery } from "~/composables/queries";
 
-const { data, hasNextPage, fetchNextPage } = useInfiniteQuery(
-    API.getPackages({}, { sortBy: "name", order: "asc" }),
-);
+const query = API.getPackages({}, { sortBy: "name", order: "asc" });
 
-const items = computed(() => 
-    data.value?.pages.map(({ items }) => items).flat(),
-);
-const skeletons = computed(() => hasNextPage ? [1, 2] : [])
-const _ = useInfiniteScroll(
-    //TODO Make this cleaner
-    document ? document.getElementById("el") : undefined,
-    async () => {
-        if (hasNextPage) {
-            await fetchNextPage();
-        }
-    },
-    { distance: 100, canLoadMore: () => hasNextPage.value },
-);
 </script>
 <template>
-    <div class="w-full h-auto flex justify-center">
-        <div class="poster-grid h-auto max-w-screen-xl p-2">
-            <PackageItem
-                :package="item"
-                v-for="item in items"
-                :key="item.id"
-                class="h-full"
-            />
-            <PackageItem
-                :package="undefined"
-                v-if="hasNextPage"
-                v-for="_ in skeletons"
-                class="h-full"
-            />
-        </div>
-    </div>
+    <InfiniteScroll :query="query" type="poster" v-slot="{ item }">
+        <PackageItem :package="item" class="h-full" />
+    </InfiniteScroll>
 </template>
